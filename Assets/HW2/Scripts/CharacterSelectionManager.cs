f371c3e4de330fa169522e6e68a107724d29863c
cN3rd <subs.in.tokyo@gmail.com>
2025-06-14 18:58:16 +0300

    Use runner in provided scene

--- Assets/HW2/Scripts/CharacterSelectionManager.cs
+++ Assets/HW2/Scripts/CharacterSelectionManager.cs
@@ -1,94 +1,87 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
 using Fusion;
-using HW2.Scripts;
-public class CharacterSelectionManager : NetworkBehaviour
+using UnityEngine.SceneManagement;
+
+namespace HW2.Scripts
 {
-    [Header("HUD Stuff")]
-    [SerializeField] List<CharacterButton> selectButtons;
-    [SerializeField] RectTransform charcterButtonParent;
-
-    [Header("Player Stuff")]
-    [SerializeField] CharacterCapsule playerCapsulePrefab;
-
-    private HashSet<Color> takenCharactersColor = new HashSet<Color>();
-
-    private NetworkRunner _runnerSession; // For Debugging - This object need to be spawned on load scene or conctted there
-    private Vector3[] positions = new Vector3[] // Temporery positions that the Players can spawn. Need to be calculted like what Lior did at the class
+    public class CharacterSelectionManager : NetworkBehaviour
     {
-    new Vector3(-2, 0, 0),
-    new Vector3(0, 0, 2),
-    new Vector3(2, 0, -2)
-    };
+        [Header("HUD Stuff")] [SerializeField] List<CharacterButton> selectButtons;
+        [SerializeField] RectTransform charcterButtonParent;
 
-    private void OnEnable()
-    {
-        foreach (CharacterButton button in selectButtons)
+        [Header("Player Stuff")] [SerializeField]
+        CharacterCapsule playerCapsulePrefab;
+
+        private readonly HashSet<Color> _takenCharactersColor = new();
+        private NetworkRunner _runnerSession;
+        private readonly Vector3[] _positions = { new(-2, 0, 0), new(0, 0, 2), new(2, 0, -2) };
+
+        private void OnEnable()
         {
-            button.OnButtonClicked += (Color selectedColor) =>
+            foreach (CharacterButton button in selectButtons)
             {
-                Vector3 randomSpawn = positions[Random.Range(0, positions.Length)];
-                Rpc_RequestCharacterSelection(selectedColor, randomSpawn);
-            };
+                button.OnButtonClicked += (Color selectedColor) =>
+                {
+                    Vector3 randomSpawn = _positions[Random.Range(0, _positions.Length)];
+                    Rpc_RequestCharacterSelection(selectedColor, randomSpawn);
+                };
+            }
         }
-    }
-    public void AssignNetwork(NetworkRunner runner)
-    {
-        _runnerSession = runner; 
-    }
 
-    [Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.StateAuthority)]
-    public void Rpc_RequestCharacterSelection(Color characterColor, Vector3 spawnPosition, RpcInfo info = default)
-    {
-        if (!takenCharactersColor.Contains(characterColor))
+        private void Start() =>
+            _runnerSession = NetworkRunner.GetRunnerForScene(SceneManager.GetActiveScene());
+
+        [Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.StateAuthority)]
+        public void Rpc_RequestCharacterSelection(Color characterColor, Vector3 spawnPosition,
+            RpcInfo info = default)
         {
-            takenCharactersColor.Add(characterColor); // Adding the color if there isnt same color there
-
-            // Approve
-            RPC_CharacterApproved(info, characterColor, spawnPosition);
-        }
-        else
-        {
-            // Deny
-            RPC_CharacterDenied(info); // Color already exits so you cant take that character
-        }
-    }
-
-    [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.InputAuthority)]
-    private void RPC_CharacterApproved(RpcInfo runner, Color characterColor, Vector3 spawnPosition)
-    {
-        Debug.Log($"Character {characterColor} approved. Spawning...");
-
-        // Spawn Logic :
-        SpawnPlayer(characterColor);
-    }
-
-    [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.InputAuthority)]
-    private void RPC_CharacterDenied(RpcInfo runner)
-    {
-        Debug.Log("Character already taken. Choose another.");
-
-        // Trigger UI update or retry
-    }
-
-    private void SpawnPlayer(Color color)
-    {
-        CharacterCapsule playerToSpawn = _runnerSession.Spawn(playerCapsulePrefab);
-        playerToSpawn.SetColor(color);
-    }
-
-
-    private void OnValidate()
-    {
-        if (selectButtons.Count <= 0 && charcterButtonParent)
-        {
-            foreach (RectTransform child in charcterButtonParent)
+            if (_takenCharactersColor.Add(characterColor))
             {
-                CharacterButton characterButton = child.GetComponent<CharacterButton>();
-                selectButtons.Add(characterButton);
+                // Adding the color if there isnt same color there
+                // Approve
+                RPC_CharacterApproved(info, characterColor, spawnPosition);
+            }
+            else
+            {
+                // Deny
+                RPC_CharacterDenied(info); // Color already exits so you cant take that character
+            }
+        }
+
+        [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.InputAuthority)]
+        private void RPC_CharacterApproved(RpcInfo runner, Color characterColor, Vector3 spawnPosition)
+        {
+            Debug.Log($"Character {characterColor} approved. Spawning...");
+
+            // Spawn Logic :
+            SpawnPlayer(characterColor);
+        }
+
+        [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.InputAuthority)]
+        private void RPC_CharacterDenied(RpcInfo runner)
+        {
+            Debug.Log("Character already taken. Choose another.");
+
+            // Trigger UI update or retry
+        }
+
+        private void SpawnPlayer(Color color)
+        {
+            CharacterCapsule playerToSpawn = _runnerSession.Spawn(playerCapsulePrefab);
+            playerToSpawn.SetColor(color);
+        }
+
+        private void OnValidate()
+        {
+            if (selectButtons.Count <= 0 && charcterButtonParent)
+            {
+                foreach (RectTransform child in charcterButtonParent)
+                {
+                    CharacterButton characterButton = child.GetComponent<CharacterButton>();
+                    selectButtons.Add(characterButton);
+                }
             }
         }
     }
-
 }
