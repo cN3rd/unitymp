79649e42db901d57dfb466ae47605a19a559bb00
idandorany <idan98765432123@gmail.com>
2025-06-15 20:04:39 +0300

    add chat
    add chat

--- Assets/HW2/Scripts/PlayerData.cs
+++ Assets/HW2/Scripts/PlayerData.cs
@@ -1,4 +1,6 @@
 using Fusion;
+using TMPro;
+using UnityEngine;
 
 namespace HW2.Scripts
 {
@@ -6,8 +8,39 @@ namespace HW2.Scripts
     {
         [Networked, OnChangedRender(nameof(UpdatePlayerNickname))] public NetworkString<_32> Nickname { get; set; }
 
-        public override void Spawned() => Runner.MakeDontDestroyOnLoad(gameObject);
+        [SerializeField] private GameObject chatUIPrefab;
+        private ChatManager localChat;
+
+        public override void Spawned()
+        {
+            Runner.MakeDontDestroyOnLoad(gameObject);
+
+            if (Object.HasInputAuthority)
+            {
+                GameObject chatUI = Instantiate(chatUIPrefab);
+                localChat = chatUI.GetComponent<ChatManager>();
+                localChat.SetOwner(this); //  Link to PlayerData for sending messages
+            }
+        }
 
         private void UpdatePlayerNickname() => PlayerNameContainer.Instance?.NotifyPlayerNameChangeRPC(Nickname);
+
+        // This is called by ChatManager when Enter is pressed and a message is typed
+        public void SendChatMessage(string msg)
+        {
+            if (!string.IsNullOrWhiteSpace(msg))
+            {
+                Rpc_DeliverChatMessage(msg);
+            }
+        }
+
+        [Rpc(RpcSources.InputAuthority, RpcTargets.All)]
+        private void Rpc_DeliverChatMessage(string msg, RpcInfo info = default)
+        {
+            foreach (ChatManager chat in FindObjectsOfType<ChatManager>())
+            {
+                chat.ReceiveMessageLocally($"Player {info.Source.PlayerId}: {msg}");
+            }
+        }
     }
 }
