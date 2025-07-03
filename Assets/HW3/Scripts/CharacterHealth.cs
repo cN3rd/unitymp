e4126b47d499f58d63f7a8671deb1122f6a855e1
cN3rd <subs.in.tokyo@gmail.com>
2025-07-03 16:27:06 +0300

    Add character HP component

---
+++ Assets/HW3/Scripts/CharacterHealth.cs
@@ -0,0 +1,42 @@
+using System;
+using Fusion;
+using UnityEngine;
+
+namespace HW3.Scripts
+{
+    public class CharacterHealth : NetworkBehaviour
+    {
+        [SerializeField] private int maxHealth = 100;
+
+        [Networked]
+        [OnChangedRender(nameof(OnHealthValueChanged))]
+        public int CurrentHealth { get; set; } = 1;
+<
+        public event Action<float> OnHealthChanged;
+        public event Action OnDeath;
+
+        public override void Spawned()
+        {
+            if (HasStateAuthority)
+                CurrentHealth = maxHealth;
+        }
+
+        [Rpc]
+        public void TakeDamageRPC(int damage) => TakeDamage(damage);
+
+        private void TakeDamage(int damage)
+        {
+            if (HasStateAuthority)
+                CurrentHealth -= damage;
+
+            if (CurrentHealth <= 0)
+                OnDeath?.Invoke();
+        }
+
+        private void OnHealthValueChanged()
+        {
+            float fraction = CurrentHealth / (float)maxHealth;
+            OnHealthChanged?.Invoke(fraction);
+        }
+    }
+}
