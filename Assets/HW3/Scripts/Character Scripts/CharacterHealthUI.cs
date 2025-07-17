c2681dc61318c98cad327a530e6cfaa51866bdfb
cN3rd <subs.in.tokyo@gmail.com>
2025-07-17 23:12:03 +0300

    Add health UI

---
+++ Assets/HW3/Scripts/Character Scripts/CharacterHealthUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HW3.Scripts
+{
+    public class CharacterHealthUI : MonoBehaviour
+    {
+        [SerializeField] private CharacterHealth healthComponent;
+        [SerializeField] private Slider healthSlider;
+
+        void Awake() => healthComponent.OnHealthChanged += HealthChanged;
+
+        private void HealthChanged(float newFraction) => healthSlider.value = newFraction;
+    }
+}
